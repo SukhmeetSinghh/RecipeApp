e8bf8387d3a942bea07e76028801de96e84af23e
Harmanjot Singh Khehra <kheh0003@algonquinlive.com>
2022-04-08 17:26:40 -0400

    Custom adapter second commit

--- app/src/main/java/com/cst2335/recipeapp/adapter/CustomAdapter.java
+++ app/src/main/java/com/cst2335/recipeapp/adapter/CustomAdapter.java
@@ -1,6 +1,5 @@
 package com.cst2335.recipeapp.adapter;
 
-import java.util.ArrayList;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,39 +15,37 @@ import java.util.ArrayList;
 import recipe.app.model.Result;
 import recipe.app.R;
 
-public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> implements Filterable{
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> implements Filterable {
 
     private ArrayList<Result> list;
     private ArrayList<Result> filteredList;
     private Context context;
     private CustomItemClickListener customItemClickListener;
 
-    public CustomAdapter(Context context, ArrayList<Result> result ArrayList, CustomItemClickListener customItemClickListener)
-    {
-        this.context=context;
+    public CustomAdapter(Context context, ArrayList<Result> resultArrayList, CustomItemClickListener customItemClickListener) {
+        this.context = context;
         this.list = resultArrayList;
         this.filteredList = resultArrayList;
         this.customItemClickListener = customItemClickListener;
     }
 
     @Override
-    public CustomAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i)
-    {
+    public CustomAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
         final MyViewHolder myViewHolder = new MyViewHolder(view);
         view.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
+                // for click item listener
                 customItemClickListener.onItemClick(filteredList.get(myViewHolder.getAdapterPosition()), myViewHolder.getAdapterPosition());
             }
-
         });
         return myViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(CustomAdapter.MyViewHolder viewHolder, int position)
-    {
+    public void onBindViewHolder(CustomAdapter.MyViewHolder viewHolder, int position) {
+
         viewHolder.title.setText(filteredList.get(position).getTitle());
     }
 
@@ -58,22 +55,33 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     }
 
     @Override
-    public Filter getFilter()
-    {
+    public Filter getFilter() {
+
         return new Filter() {
             @Override
             protected FilterResults performFiltering(CharSequence charSequence) {
-                filteredList =list;
+
+                filteredList = list;
 
                 FilterResults filterResults = new FilterResults();
                 filterResults.values = filteredList;
                 return filterResults;
-
             }
 
-
-
-        }
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredList = (ArrayList<Result>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.title);
+        }
+    }
 }
